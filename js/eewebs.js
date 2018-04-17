b68cae87e7ec46febdee1790affe67b94a5cd97d
Atilla Lonny <contact.gh@atil.la>
2018-04-17 18:26:27 -0400

    Refactoring JS - ty jesse c:

--- js/eewebs.js
+++ js/eewebs.js
@@ -1,5 +1,3 @@
-    let conn = null;
-    let signs = null;
     let world_id = window.location.hash.substr(1);
 
     if (!window.location.hash)
@@ -10,41 +8,30 @@
             client.bigDB.load("config", "config", config => {
                 client.multiplayer.useSecureConnections = true;
                 client.multiplayer.createJoinRoom(world_id, "Everybodyedits" + config.version, true, null, null, connection => {
-                    conn = connection;
-                    connection.addMessageCallback("*", on_message);
+                    connection.addMessageCallback("init", function(e) {
+                        connection.disconnect();
+
+                       let bldatam = jsparse(e);
+                       let signs = bldatam.filter(block => block.id == 385)
+                                          .sort(function(a, b) { return a.x > b.x ? 1 : 0; })
+                                          .sort(function(a, b) { return a.y > b.y ? 1 : 0; });
+                        
+                        let html = signs.map(sign => sign.args[1]).join('');
+                        let iframe = document.createElement('iframe');
+
+                        iframe.width = '100%';
+                        iframe.height = '100%';
+                        iframe.frameBorder = '0';
+                
+                        document.body.getElementsByTagName('div')[0].appendChild(iframe);
+                        iframe.contentWindow.document.open();
+                        iframe.contentWindow.document.write(html);
+                        iframe.contentWindow.document.close();
+                    });
                     connection.send("init");
                 }, callback_error);
             }, callback_error);
         }, callback_error);
-    }
-
-    function on_message(e) {
-        if (e.type != "init")
-            return;
-
-        conn.disconnect();
-
-        let bldatam = jsparse(e);
-        let signs = bldatam.filter(block => block.id == 385)
-                           .sort(function(a, b) { return a.x > b.x ? 1 : 0; })
-                           .sort(function(a, b) { return a.y > b.y ? 1 : 0; });
-
-        let html = '';
-
-        signs.forEach(sign => {
-            html += sign.args[1];
-        });
-
-        let iframe = document.createElement('iframe');
-
-        iframe.width = '100%';
-        iframe.height = '100%';
-        iframe.frameBorder = '0';
-
-        document.body.getElementsByTagName('div')[0].appendChild(iframe);
-        iframe.contentWindow.document.open();
-        iframe.contentWindow.document.write(html);
-        iframe.contentWindow.document.close();
     }
 
     function callback_error(error) { console.log("error: " + error.code + ": " + error.message); }
