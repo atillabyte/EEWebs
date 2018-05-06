b41edf90efdd4b1f7dd3fb1bbb74b4a03b022508
Atilla Lonny <contact.gh@atil.la>
2018-05-06 16:15:39 -0400

    (accidentally unstaged)

--- publish.sh
+++ publish.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # yarn add google-closure-compiler
 
-yarn google-closure-compiler --js=js/*.js --js_output_file=dist/eewebs.min.js
+rm -rf 'dist/eewebs.min.js'
+yarn google-closure-compiler --rewrite_polyfills=false --compilation_level=SIMPLE --js=js/eewebs.js --js=js/init.js --js_output_file=dist/eewebs.min.js
